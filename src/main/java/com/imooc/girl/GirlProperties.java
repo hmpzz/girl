ae0ca363b3924b731d643d35b28fd80d382cf161
DESKTOP-OOOPCQG\zhouyuqing <969243585@qq.com>
2017-09-08 23:57:25 +0800

    第一天java学习加入的代码

---
+++ src/main/java/com/imooc/girl/GirlProperties.java
@@ -0,0 +1,28 @@
+package com.imooc.girl;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component //想要被别的类引用需要加这行注解
+@ConfigurationProperties(prefix = "girl")  //此名表示使用配置文件中的girl部分
+public class GirlProperties {
+
+    private String cupSize;
+    private int age;
+
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/imooc/girl/HelloController.java
@@ -0,0 +1,22 @@
+package com.imooc.girl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController  //用WEB的方式访问类
+public class HelloController {
+
+    @Autowired //使用配置类需要加这行注解
+    private GirlProperties girlproperties;
+
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET) //访问的目录，用GET的方式访问
+    public String Say(){
+
+        return girlproperties.getCupSize();
+    }
+}
