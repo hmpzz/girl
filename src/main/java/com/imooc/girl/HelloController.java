666e79de0ea60ecdcc4e6d1f1065b79cc11e2f96
DESKTOP-OOOPCQG\zhouyuqing <969243585@qq.com>
2017-09-10 12:13:33 +0800

    第一天java学习加入的代码

--- src/main/java/com/imooc/girl/HelloController.java
+++ src/main/java/com/imooc/girl/HelloController.java
@@ -2,9 +2,7 @@ package com.imooc.girl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController  //用WEB的方式访问类
@@ -14,9 +12,9 @@ public class HelloController {
     private GirlProperties girlproperties;
 
 
-    @RequestMapping(value = {"/hello","/hi"},method = RequestMethod.POST) //访问的目录，用GET的方式访问
-    public String Say(){
-
-        return girlproperties.getCupSize();
+    @RequestMapping(value = "/say",method = RequestMethod.GET) //访问的目录，用GET的方式访问
+    public String Say(@RequestParam(value = "id",required =false,defaultValue = "888") int id){
+            return "ID:"+id;
+//        return girlproperties.getCupSize();
     }
 }
